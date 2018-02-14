59d2cbc616d879a17dd2151facc855ecdbeeba0f
Sarah <velaniab@aol.com>
2018-02-15 00:56:28 +0100

    moves line handling in ChemCompilerFolder to its own method

--- CompileChems/CompileChems/ChemCompilerFolder.cs
+++ CompileChems/CompileChems/ChemCompilerFolder.cs
@@ -8,9 +8,12 @@ using System.Text.RegularExpressions;
 
 namespace CompileChems {
     static class ChemCompilerFolder {
+        private static Dictionary<string, int> _ckeyDic = new Dictionary<string, int>();
+        private static string _name;
+        private static string _dose;
+
         public static void CompileChems() {
             StreamReader sr;
-            Dictionary<string, int> ckeyDic = new Dictionary<string, int>();
             List<string> resultList = new List<string>();
             //get folder name from user
             string path = FileAccessing.GetFolderPath();
@@ -23,37 +26,16 @@ namespace CompileChems {
 
 
 
-            string patternReagent = "(?<=u of )" + reagentName + " have been"; // (?<=u of )reagentName have been               matches to "u of " and "reagentname" and "have been" in order
-            string patternName = "((?<=last touched by )|(?<=carried by )).+$"; // ((?<=last touched by )|(?<=carried by )).+$  matches 1 or more chars at the end of the string after "last touched by " OR "carried by "
-            string patternDose = "(?<=\\|\\|\\s)\\d+"; // (?<=\|\|\s)\d+                                                        matches one or more digits after "|| "
-            Regex rgxReagent = new Regex(patternReagent);
 
-            string line;
-            string name;
-            string dose;
             foreach(string file in files) {
                 string filePath = path + $"{file}";
                 sr = new StreamReader(filePath);
 
-                while((line = sr.ReadLine()) != null) {
-                    Match matchReagent = rgxReagent.Match(line);
-                    if(matchReagent.Success) {
-                        name = MatchString(line, patternName); //match name
-                        dose = MatchString(line, patternDose); //match reagent dose
+                DoLines(reagentName, sr);
 
-                        //add to dic
-                        if(Int32.TryParse(dose, out int doseInt)) {
-                            if(!ckeyDic.ContainsKey(name)) {
-                                ckeyDic.Add(name, doseInt);
-                            } else {
-                                ckeyDic[name] = ckeyDic[name] + doseInt;
-                            }
-                        }
-                    }
-                }
                 sr.Close();
             }
-            var ordered = ckeyDic.OrderByDescending(x => x.Value); //sort dictionary by amount of reagent created
+            var ordered = _ckeyDic.OrderByDescending(x => x.Value); //sort dictionary by amount of reagent created
             //add gathered dictionary of ckeys vs created amount to list for writing
             foreach(KeyValuePair<string, int> kvp in ordered) {
                 resultList.Add($"{kvp.Key} created {kvp.Value}u {reagentName}");
@@ -61,6 +43,31 @@ namespace CompileChems {
 
             FileAccessing.WriteToFile(resultList);
             Console.WriteLine("Streams closed.");
+        }
+
+        private static void DoLines(string reagentName, StreamReader sr) {
+            string line;
+            string patternReagent = "(?<=u of )" + reagentName + " have been"; // (?<=u of )reagentName have been               matches to "u of " and "reagentname" and "have been" in order
+            string patternName = "((?<=last touched by )|(?<=carried by )).+$"; // ((?<=last touched by )|(?<=carried by )).+$  matches 1 or more chars at the end of the string after "last touched by " OR "carried by "
+            string patternDose = "(?<=\\|\\|\\s)\\d+"; // (?<=\|\|\s)\d+                                                        matches one or more digits after "|| "
+            Regex rgxReagent = new Regex(patternReagent);
+
+            while ((line = sr.ReadLine()) != null) {
+                Match matchReagent = rgxReagent.Match(line);
+                if(matchReagent.Success) {
+                    _name = MatchString(line, patternName); //match name
+                    _dose = MatchString(line, patternDose); //match reagent dose
+
+                    //add to dic
+                    if(Int32.TryParse(_dose, out int doseInt)) {
+                        if(!_ckeyDic.ContainsKey(_name)) {
+                            _ckeyDic.Add(_name, doseInt);
+                        } else {
+                            _ckeyDic[_name] = _ckeyDic[_name] + doseInt;
+                        }
+                    }
+                }
+            }
         }
 
         private static string MatchString(string line, string pattern) {
