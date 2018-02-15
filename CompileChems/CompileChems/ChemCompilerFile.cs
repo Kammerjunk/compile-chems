4f3f57c2c0d23f461bb6d7d4dd7936547c3e2a5a
Sarah <velaniab@aol.com>
2018-02-15 01:17:47 +0100

    adds child of ChemCompilerBase for single-file handling

---
+++ CompileChems/CompileChems/ChemCompilerFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompileChems {
+    class ChemCompilerFile : ChemCompilerBase {
+        public void CompileChems() {
+            //get file name from user
+            StreamReader sr = FileAccessing.GetFilePath();
+
+            //get reagent name from user
+            _reagentName = GetReagentName();
+
+            //process reagents
+            DoLines(_reagentName, _sr);
+            _sr.Close();
+
+            //write to file
+            FileAccessing.WriteToFile(_resultList);
+            Console.WriteLine("Streams closed.");
+        }
+    }
+}
